4e900a643c62186bfc18b1b51422b5c9a11f50df
teo5253 <mateolangston@gmail.com>
2015-02-24 16:43:29 -0800

    b

---
+++ clicks/clicks.js
@@ -0,0 +1,11 @@
+/*global $*/
+/*jslint sloppy:true, browser: true, white: true*/
+$("h1.page-title").text("My code runs!");
+$(window).on('click', function (e) {
+    $('img.logo').css({left: e.pageX,
+                       top: e.pageY});
+});
+
+
+
+
--- javascript_clicks/clicks.js
+++
@@ -1,3 +0,0 @@
-/*global $*/
-/*jslint sloppy:true, browser: true, white: true*/
-$("h1.page-title").text("My code runs!");
