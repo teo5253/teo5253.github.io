33bc312846dbf60a41f1e84c314a364f3afe7bb4
Mateo David Langston Smith <malangstonsmith@Mateos-MBP.lan>
2016-02-06 15:22:54 -0800

    h

--- sixMonths/script.js
+++ sixMonths/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('div').click(function() {
-        $(this).addClass('blackwhite');
+        $(this).html('<img href="firstpic.jpeg" />');
         };
     });
 });
